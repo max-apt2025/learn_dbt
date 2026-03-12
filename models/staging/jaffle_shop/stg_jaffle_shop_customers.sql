b7dc56ee35e8766c88ec3f9d517327d2229905e3
max challa <fennec_7ploy@icloud.com>
2026-03-12 07:02:10 +0000

    migrated and refactored the CTE into dbt sources, marts and modesl

---
+++ models/staging/jaffle_shop/stg_jaffle_shop_customers.sql
@@ -0,0 +1,21 @@
+{{ config(materialized='table') }}
+
+with source as (
+
+    select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+transformed as (
+
+    select
+        id as customer_id,
+        first_name ,
+        last_name ,
+        first_name ||' ' || last_name as full_name
+
+    from source
+
+)
+
+select * from transformed
